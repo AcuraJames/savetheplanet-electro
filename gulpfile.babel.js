448d92f26a93f8e4db9f613ebca233d16216d816
saitama <snusmoomroomia2020@gmail.com>
2019-04-07 14:18:42 +0900

    dev/prod env

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,44 +3,57 @@ import sass from 'gulp-sass'
 import rev from 'gulp-rev'
 import revReplace from 'gulp-rev-replace'
 import autoprefixer from 'gulp-autoprefixer'
+import del from 'del'
+import {argv} from 'yargs'
+import _if from 'gulp-if' 
 import cssnano from 'gulp-cssnano'
 import imagemin from 'gulp-imagemin'
+import babel from 'gulp-babel'
 import uglify from 'gulp-uglify'
 import {create as bsCreate} from 'browser-sync'
 
 const browserSync = bsCreate()
 
+const prod = argv.env === 'production'
+
+gulp.task('clean', () => {
+    return del(['manifest', 'public/{styles, js}'])
+})
 
 gulp.task('styles', () => {
     return gulp.src('src/styles/main.scss')
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(cssnano())
-        .pipe(rev())
+        .pipe(_if(prod, cssnano()))
+        .pipe(_if(prod, rev()))
         .pipe(gulp.dest('public/styles'))
-        .pipe(rev.manifest('css.json'))
+        .pipe(_if(prod, rev.manifest('css.json')))
         .pipe(gulp.dest('manifest'))
 })
 
 gulp.task('index', () => {
     return gulp.src('src/index.html')
-        .pipe(revReplace({
-            manifest: gulp.src('manifest/css.json')
-        }))
+        .pipe(_if(prod, revReplace({
+            manifest: gulp.src('manifest/css.json', {allowEmpty: true})
+        })))
         .pipe(gulp.dest('public'))
 })
 
 gulp.task('js', () => {
     return gulp.src('src/js/**/*.js')
+        .pipe(_if(prod, babel({
+            presets: ['@babel/env']
+        })))
+        .pipe(_if(prod, uglify()))
         .pipe(gulp.dest('public/js'))
 })
 
 gulp.task('img', () => {
     return gulp.src('src/img/**/*.*')
-        .pipe(imagemin([
+        .pipe(_if(prod, imagemin([
             imagemin.jpegtran({progressive: true}),
             imagemin.optipng({optimizationLevel: 5})
-        ]))
+        ])))
         .pipe(gulp.dest('public/img'))
 })
 
@@ -55,11 +68,10 @@ gulp.task('watch', () => {
 
 gulp.task('serve', () => {
     browserSync.init({
-        server: 'public',
-        tunnel: true
+        server: 'public'
     })
     browserSync.watch('public/**/*.*').on('change', browserSync.reload)
 })
 
-gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')))
+gulp.task('dev', gulp.series('clean', 'build', gulp.parallel('watch', 'serve')))
 
